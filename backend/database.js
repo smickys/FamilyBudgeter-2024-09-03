e057bda23c0f854ad6109c0281bb68be59b26396
Simonas Mickys <simonas.mickys@gmail.com>
2024-09-03 20:30:35 +0300

    userschema created

--- backend/database.js
+++ backend/database.js
@@ -11,6 +11,33 @@ const db = mongoose.connection
 db.on('error', () => console.log('Failed to connect to MongoDB'))
 db.once('open', () => console.log('Connected to MongoDB'))
 
+// USERS schema
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    created_at: { type: String }
+})
+
+const User = mongoose.model('User', userSchema)
+
+// // WALLET schema
+// const walletSchema = new mongoose.Schema({
+//     name: { type: String, required: true },
+//     created_at: { type: String },
+//     archived: { type: Boolean, default: false },
+//     users: [
+//         {
+//             user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+//             role: { type: String, enum: ['Administrator', 'Moderator'], required: true }
+//         }
+//     ]
+// })
+
+// const Wallet = mongoose.model('Wallet', walletSchema)
+
 module.exports = {
-    db
+    db,
+    mongoose,
+    User
 }
